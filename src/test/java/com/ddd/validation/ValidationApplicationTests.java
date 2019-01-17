be022f3a9411637855a10bb74b0e5ded35e3795f
Zbigniew Artemiuk <zbigniew.artemiuk@pragmatists.pl>
2019-01-17 09:34:42 +0100

    setup project

---
+++ src/test/java/com/ddd/validation/ValidationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ddd.validation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ValidationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
